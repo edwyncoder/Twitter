dad6a985d131c6d1b730791982e884f1272c8401
edwyncoder <edwyn.t@flyerssoft.com>
2023-10-16 22:11:46 +0530

    added logic for toggle likes

--- src/repository/index.js
+++ src/repository/index.js
@@ -1,6 +1,8 @@
 import TweetRepository from './tweet-repository.js'
 import HashtagRepository from './hashtag-repository.js'
+import LikeRespository from './like-repository.js'
 export {
     TweetRepository,
     HashtagRepository,
+    LikeRespository
 }
