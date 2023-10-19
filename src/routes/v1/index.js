fb30d9187bcbe984c5a7df5655f33b3d3d47b41a
edwyncoder <edwyn.t@flyerssoft.com>
2023-10-19 20:45:54 +0530

    added apis for comments

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,10 +2,13 @@ import express from 'express';
 
 import { createTweet } from '../../controllers/tweet-controller.js';
 import { toggleLike } from '../../controllers/like-controller.js';
+import { createComment } from '../../controllers/comment-controller.js';
 
 const router = express.Router();
 
 router.post('/tweets', createTweet);
+
+router.post('/comments', createComment);
 
 router.post('/likes/toggle',toggleLike);
 
