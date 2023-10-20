9c540709cfd1ebe7682fa0e000ae0a11349fe4b6
edwyncoder <edwyn.t@flyerssoft.com>
2023-10-20 22:53:15 +0530

    added signup for users

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,16 +1,20 @@
 import express from 'express';
 
-import { createTweet } from '../../controllers/tweet-controller.js';
+import { createTweet, getTweet } from '../../controllers/tweet-controller.js';
 import { toggleLike } from '../../controllers/like-controller.js';
 import { createComment } from '../../controllers/comment-controller.js';
+import { signup } from '../../controllers/auth-controller.js';
 
 const router = express.Router();
 
 router.post('/tweets', createTweet);
+router.get('/tweets/:id', getTweet);
 
 router.post('/comments', createComment);
 
 router.post('/likes/toggle',toggleLike);
+
+router.post('/signup', signup);
 
 export default router;
 
