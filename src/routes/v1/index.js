292cb0c6a1426c5f92247965dbee2af365bebf61
edwyncoder <edwyn.t@flyerssoft.com>
2023-10-21 12:23:28 +0530

    added login and gen token

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 import { createTweet, getTweet } from '../../controllers/tweet-controller.js';
 import { toggleLike } from '../../controllers/like-controller.js';
 import { createComment } from '../../controllers/comment-controller.js';
-import { signup } from '../../controllers/auth-controller.js';
+import { signup, login } from '../../controllers/auth-controller.js';
 
 const router = express.Router();
 
@@ -15,6 +15,7 @@ router.post('/comments', createComment);
 router.post('/likes/toggle',toggleLike);
 
 router.post('/signup', signup);
+router.post('/login', login);
 
 export default router;
 
